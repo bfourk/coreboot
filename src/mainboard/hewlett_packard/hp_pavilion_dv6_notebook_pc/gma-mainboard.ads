e339903cdf5993cd4b6cb73f580de74c4c132f55
bfourk <100809777+bfourk@users.noreply.github.com>
2025-05-08 17:38:18 -0400

    Update Ports

--- src/mainboard/hewlett_packard/hp_pavilion_dv6_notebook_pc/gma-mainboard.ads
+++ src/mainboard/hewlett_packard/hp_pavilion_dv6_notebook_pc/gma-mainboard.ads
@@ -8,16 +8,9 @@ use HW.GFX.GMA.Display_Probing;
 
 private package GMA.Mainboard is
 
-   -- FIXME: check this
    ports : constant Port_List :=
-     (DP1,
-      DP2,
-      DP3,
-      HDMI1,
-      HDMI2,
-      HDMI3,
-      Analog,
-      LVDS,
-      eDP);
+     (HDMI1,
+      VGA1,
+      LVDS);
 
 end GMA.Mainboard;
